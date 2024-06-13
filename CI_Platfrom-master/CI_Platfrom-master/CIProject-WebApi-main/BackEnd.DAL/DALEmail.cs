e6b08f8173eb5154a537ffc643a1456ded029f1a
VedDudhat <veddudhat18@gmail.com>
2024-06-13 14:15:47 +0530

    ok

---
+++ CI_Platfrom-master/CI_Platfrom-master/CIProject-WebApi-main/BackEnd.DAL/DALEmail.cs
@@ -0,0 +1,35 @@
+﻿using BackEnd.Entity;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+
+namespace BackEnd.DAL
+{
+ 
+
+    public class DALEmail
+    {
+        public void SendEmail(EmailDto request)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Your Name", request.From));
+            message.To.Add(new MailboxAddress("Recipient", request.To));
+            message.Subject = request.Subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = request.Body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.your-email-provider.com", 587, false);
+                client.Authenticate("your-email@example.com", "your-email-password");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+
+}
